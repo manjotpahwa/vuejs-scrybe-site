d87a476521becd54f8329ac736132b57d171d59a
Manjot <manjotpahwa@gmail.com>
2020-01-22 21:25:36 +0530

    Initial dist subtree commit

---
+++ precache-manifest.a0bf07db85221d68bf9cfb7c385a8930.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff66b25a0ebf00d45f6a72f7fc265f2c",
+    "url": "/vuejs-scrybe-site/img/brand/logo.png"
+  },
+  {
+    "revision": "1dd2fce4c79c5cd8fa2a06fc30baca5d",
+    "url": "/vuejs-scrybe-site/img/brand/plots screenshot.png"
+  },
+  {
+    "revision": "26a8fe2e22a3a44abfd0",
+    "url": "/vuejs-scrybe-site/js/chunk-vendors.debe5409.js"
+  },
+  {
+    "revision": "8f23df122fc78432490934ac08e59061",
+    "url": "/vuejs-scrybe-site/img/theme/ManjotPahwa.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/vuejs-scrybe-site/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/vuejs-scrybe-site/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/vuejs-scrybe-site/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/vuejs-scrybe-site/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "a7435bbc0a1b62ea82777077cec85d50",
+    "url": "/vuejs-scrybe-site/img/header-img.png"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/vuejs-scrybe-site/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "538acb7b7372c338e37ba9b5e1abb16a",
+    "url": "/vuejs-scrybe-site/img/women_preso_discussing.png"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/vuejs-scrybe-site/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vuejs-scrybe-site/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "11c51084935cc766fca5258add98dbdc",
+    "url": "/vuejs-scrybe-site/index.html"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/vuejs-scrybe-site/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/vuejs-scrybe-site/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/vuejs-scrybe-site/img/theme/landing.jpg"
+  },
+  {
+    "revision": "71883344aa1fb0c1bac4a7d2cc6308ac",
+    "url": "/vuejs-scrybe-site/img/theme/HimanshuChaudhary.jpeg"
+  },
+  {
+    "revision": "76f0657b89251f66c80452ca4efd8797",
+    "url": "/vuejs-scrybe-site/img/theme/ChandraPrakash.jpeg"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/vuejs-scrybe-site/img/brand/white.png"
+  },
+  {
+    "revision": "ece25142241134b57c1c",
+    "url": "/vuejs-scrybe-site/js/app.e2eca527.js"
+  },
+  {
+    "revision": "a7eee6a9698ab66b0f93f931282bda6a",
+    "url": "/vuejs-scrybe-site/img/theme/ManishSachdev.jpeg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/vuejs-scrybe-site/img/theme/promo-1.png"
+  },
+  {
+    "revision": "39d48b47ddec702a2a0708e025a5507f",
+    "url": "/vuejs-scrybe-site/img/theme/ManjotPahwaSquare.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/vuejs-scrybe-site/img/theme/profile.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vuejs-scrybe-site/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/vuejs-scrybe-site/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vuejs-scrybe-site/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c47104936cd781b936927cc1fc33543b",
+    "url": "/vuejs-scrybe-site/img/brand/favicon.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vuejs-scrybe-site/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/vuejs-scrybe-site/img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/vuejs-scrybe-site/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "15689451f4887312a8fec873746f82f2",
+    "url": "/vuejs-scrybe-site/img/2women_humaaans_discuss.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/vuejs-scrybe-site/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/vuejs-scrybe-site/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/vuejs-scrybe-site/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/vuejs-scrybe-site/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vuejs-scrybe-site/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c47104936cd781b936927cc1fc33543b",
+    "url": "/vuejs-scrybe-site/favicon.png"
+  },
+  {
+    "revision": "ece25142241134b57c1c",
+    "url": "/vuejs-scrybe-site/css/app.b96dbc70.css"
+  }
+];

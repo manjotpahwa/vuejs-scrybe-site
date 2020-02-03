bdc8fa5516b951b087d76fc7118cd382638bc636
Manjot <manjotpahwa@gmail.com>
2020-02-03 19:39:51 +0530

    Removing extraneous link

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -6,11 +6,6 @@
             </router-link>
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
-                <div class="col-6 collapse-brand">
-                    <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/">
-                        <img src="img/brand/blue.png">
-                    </a>
-                </div>
                 <div class="col-6 collapse-close">
                     <close-button @click="closeMenu"></close-button>
                 </div>
